2984639cf52b7845995783c0280a326d3bc6f01b
bennyhinnmathew <benny.mathew@student.moringaschool.com>
2023-12-06 12:48:13 +0300

    Defined All Bots state & added the useEffect for fetching the data

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import BotsPage from "./BotsPage";
 
 function App() {
+  const [allBots, setAllBots] = useState()
+  console.log(allBots)
+
+  useState(() => {
+    fetch("http://localhost:8002/bots")
+      .then(response => response.json())
+      .then(data => {
+        setAllBots(data)
+      })
+  })
+
+
   return (
     <div className="App">
       <BotsPage />
